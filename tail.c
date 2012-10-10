ce8ef4c0072ab3bcd7d15af2e3b576cd40b27303
U-SYSTEMS\BProcto <BProcto@SEAWSWI7BPROCTO.systems.corbis.net>
2012-10-10 11:16:34 -0700

    wip, quick implentation of tail, needs testing

--- tail.c
+++ tail.c
@@ -18,6 +18,41 @@ static char *str_usage =
 
 int command_tail(int argc, char **argv)
 {
-	usage(str_usage);
+	char *buf, *ptr;
+	int i, count;
+	int lines = 10, line_number = 0;
+	FILE *fp;
+
+	if (argc > 1) {
+		if (strcmp(argv[1], "--help") == 0) {
+			usage(str_usage);
+		} else if (strcmp(argv[1], "--version") == 0) {
+			version();
+		} else {
+			fp = fopen(argv[1], "r");
+			if (fp == NULL) {
+				printf("tail: %s: file does not exist", argv[1]);
+				return EXIT_FAILURE;
+			}
+		}
+	} else {
+		fp = stdin;
+	}
+
+	buf = malloc(4096);
+	while (!feof(fp) || line_number < lines) {
+		count = fread(buf, 4096, 1, fp);
+		ptr = buf;
+		while (*ptr != '\0') {
+			if (*ptr++ == '\n') {
+				if (++line_number == lines) {
+					*ptr = '\0';
+					break;
+				}
+			}
+		}
+		puts(buf);
+	}
+
 	return EXIT_SUCCESS;
 }
